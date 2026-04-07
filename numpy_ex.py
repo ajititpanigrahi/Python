ebaad4673403da355f47804db97da2ccd8b5cb6f
Ajit Panigrahi <akpanigrahi.2k8@gmail.com>
2026-04-07 20:03:10 +0530

    numpy examples

--- numpy_ex.py
+++ numpy_ex.py
@@ -6,18 +6,23 @@
 # muti-dimensional lists are difficult in python and very eazy in numpy
 import numpy as np
 
-list1 = np.array([1,2,3,4,5,6])
+# list1 = np.array([1,2,3,4,5,6])
+# print(list1)
+#
+# list2 = np.array([1,2,3],[4,5,6],[7,8,9])
+# print(list2)
+# print(list2.ndim)
+# print(list2.shape)
+# print(list2.size)
+# print(list2.dtype)
+# print(list2.itemsize)
+
+
+#Example -5
+
+list1 = np.arange(6)
 print(list1)
 
-list2 = np.array([1,2,3],
-                 [4,5,6],
-                 [7,8,9])
+list2 = list1.reshape(2,3)
 print(list2)
-print(list2.ndim)
-print(list2.shape)
-print(list2.size)
-print(list2.dtype)
-print(list2.itemsize)
-
-
 
