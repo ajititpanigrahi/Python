72d3dbea32c2d46c632430e1cbd4df3823e8c8d7
Ajit Panigrahi <akpanigrahi.2k8@gmail.com>
2026-04-10 19:44:23 +0530

    pandas example

--- pandas_ex.py
+++ pandas_ex.py
@@ -63,10 +63,60 @@ import pandas as pd
 # print(res)
 
 #Ex 8
-df = pd.read_csv("pandas_nulls.csv")
-print(df.isnull())
-print(df.notnull())
-print(df.dropna())
-print(df.fillna(0))
+# df = pd.read_csv("pandas_nulls.csv")
+# print(df.isnull())
+# print(df.notnull())
+# print(df.dropna())
+# print(df.fillna(0))
+
+#Ex 9
+# df = pd.read_csv("student_data.csv")
+# print(df.sort_values("Marks", ascending=False))
+
+#Ex 10
+# data = {
+#     "Dept" : ["IT","IT","HR","HR"],
+#     "Salary" : [50000,60000,45000,40000]
+# }
+# df = pd.DataFrame(data)
+# print(df.groupby("Dept")["Salary"].sum())
+# print(df.groupby("Dept")["Salary"].mean())
+# print(df.groupby("Dept")["Salary"].median())
+# print(df.groupby("Dept")["Salary"].min())
+# print(df.groupby("Dept")["Salary"].max())
+# print(df.groupby("Dept")["Salary"].std())
+# print(df.groupby("Dept")["Salary"].describe())
+
+#Ex 11
+# df1 = pd.DataFrame({
+#     "ID" : [101,102],
+#     "Name" : ["std1","std2"]
+# })
+# df2 = pd.DataFrame({
+#     "ID" : [103,104],
+#     "Marks" : [90,95]
+# })
+# print(pd.merge(df1,df2,on="ID"))
+
+# Ex 12
+# df = pd.read_csv("student_data.csv")
+# print(df.head())
+# df["Age"] = df["Age"].apply(lambda x: x+10)
+# print(df)
+
+#Ex 13
+# df = pd.read_csv("students.csv")
+# print(df.value_counts("Marks"))
+# df.describe()
+
+#Ex 14
+data = {
+    "Name" : ["Ravi","Sita","Jhon","Ravi"],
+    "Dept" : ["IT","HR","IT","IT"],
+    "Marks" :[85,90,None,85]
+}
+df = pd.DataFrame(data)
+df["Marks"] = df["Marks"].fillna(df["Marks"].mean())
+print(df.drop_duplicates().groupby("Dept")["Marks"].mean())
 
 
