2790ac249e04a858ea7dd395f0bd984dd34a1591
Ajit Panigrahi <akpanigrahi.2k8@gmail.com>
2026-04-24 17:53:05 +0530

    tuple examples

--- set_ex.py
+++ set_ex.py
@@ -73,16 +73,16 @@
 #
 # print("".join(result))
 
-nums = [1,2,3,2,4,5,1]
-seen = set()
-duplicates = set()
-for n in nums:
-    if n in seen:
-        duplicates.add(n)
-    else:
-        seen.add(n)
-
-print(duplicates)
+# nums = [1,2,3,2,4,5,1]
+# seen = set()
+# duplicates = set()
+# for n in nums:
+#     if n in seen:
+#         duplicates.add(n)
+#     else:
+#         seen.add(n)
+#
+# print(duplicates)
 
 
 
