802c77467f937953685346c54b8fb0482147cf26
Ajit Panigrahi <akpanigrahi.2k8@gmail.com>
2026-04-28 19:41:20 +0530

    seaborn note update

--- seaborn_ex.py
+++ seaborn_ex.py
@@ -11,18 +11,37 @@ data = {
      "Age" : [18,19,20,18,21,20,19,22,21,20]
 }
 
-print(data)
-student_df = pd.DataFrame(data)
-print(student_df)
+# print(data)
+# student_df = pd.DataFrame(data)
+# print(student_df)
 
-sns.scatterplot(x="Studyhours", y="Marks", hue="Gender", data=student_df)
+# sns.scatterplot(x="Studyhours", y="Marks", hue="Gender", data=student_df)
 # plt.title("Student Hours vs Marks")
 # plt.xlabel("Student Hours")
 # plt.ylabel("Student Marks")
 # plt.show()
 
+#predefined dataset using tips
 df = sns.load_dataset("tips")
-print(df)
+print(df.head(20))
 
-sns.regplot(x="Studyhours", y="Marks", data=student_df)
+# sns.regplot(x="Studyhours", y="Marks", data=student_df)
+# plt.show()
+
+#lineplot with smoker
+sns.lineplot(x="total_bill",y="tip",style="smoker",data=df)
+plt.title("Total Bills Vs Tips")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+plt.show()
+
+#lineplot with marker
+sns.lineplot(x="total_bill",y="tip",marker="o",data=df)
+
+#average tip
+average_tip = df.groupby("day")["tip"].mean().reset_index()
+sns.lineplot(x="day",y="tip",marker="o",data=average_tip)
+plt.title("Total Bills Vs Average Tip")
+plt.xlabel("Total Bill")
+plt.ylabel("Average Tip")
 plt.show()
